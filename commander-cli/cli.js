368df27bbf0d8c8e0cfdf3f01458c05676d14928
vyex <vanessasoetjiadi@gmail.com>
2025-12-03 17:13:09 +1030

    Successfully implemented the Add function

--- commander-cli/cli.js
+++ commander-cli/cli.js
@@ -1,12 +1,32 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import { join, dirname } from 'path';
+import fs from 'fs';
 
 const program = new Command();
+
+const filePath = 'expenses.json'
+
+const data = fs.readFileSync(filePath, 'utf-8');
+let arr = JSON.parse(data);
 
 program
   .name('cli')
   .description('A simple CLI application')
   .version('1.0.0')
+program
+  .command('add <description> <amount>')
+  .description('add new expenses')
+  .action((description, amount) => {
+    console.log(arr);
+
+    arr.count = arr.count + 1;
+
+    arr.expenses.push({ ID: arr.count, Date: new Date().toLocaleDateString(), Description: description, Amount: `$${ amount }` });
+    fs.writeFileSync(filePath, JSON.stringify(arr, null, 2), 'utf-8');
+
+    console.log(`"${ description }" costed $${ amount } has been added with ID ${ arr.count }`);
+  })
 
 program.parse()
